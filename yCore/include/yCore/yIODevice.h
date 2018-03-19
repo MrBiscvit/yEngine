a25471776de69f6533db7ae30e8c63f5de901ea1
MrBiscvit <mr.biscvityt@gmail.com>
2018-03-19 19:18:56 +0100

    Add yIODevice interface

---
+++ yCore/include/yCore/yIODevice.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <yCore/yCore.h>
+
+class yCORE_API yIODevice
+{
+public:
+	enum OpenMode
+	{
+		NotOpen = 0x0,
+		ReadOnly = 0x1,
+		WriteOnly = 0x2,
+		ReadWrite = ReadOnly | WriteOnly,
+		Append = 0x4,
+		Truncate = 0x8,
+		Text = 0x10
+	};
+
+public:
+
+	virtual yint64 bytesAvailable() const { return 0; }
+	virtual yint64 bytesToWrite() const { return 0; }
+
+	virtual bool open() = 0;
+	virtual void close() = 0;
+	virtual bool isOpen() = 0;
+
+	virtual bool atEnd() const { return true; }
+
+	virtual yint64 read(char * data, yint64 maxSize) = 0;
+	virtual yint64 write(const char * data, yint64 maxSize) = 0;
+	virtual bool seek(yint64 position) = 0;
+	virtual yint64 tell() const = 0;
+	virtual yint64 size() const { return 0; }
+
+	inline bool isReadable() const { return bytesAvailable() != 0; }
+	inline bool isWritable() const { return bytesToWrite() != 0; }
+};
