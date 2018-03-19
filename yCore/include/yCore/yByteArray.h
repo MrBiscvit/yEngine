9cb8a469ae2889949646378f26791733a74decb8
MrBiscvit <mr.biscvityt@gmail.com>
2018-03-19 19:58:12 +0100

    Initial implement of yByteArray

---
+++ yCore/include/yCore/yByteArray.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include <yCore/yCore.h>
+
+class yCORE_API yByteArray
+{
+public:
+
+	inline int size() const { return _size; }
+	inline int length() const { return _size; }
+	inline int count() const { return _size; }
+	inline bool empty() const { return _size == 0; }
+	inline bool isEmpty() const { return _size == 0; }
+
+	inline char front() const { return at(0); }
+	inline char & front() { return at(0); }
+	inline char back() const { return at(_size - 1); }
+	inline char & back() { return at(_size - 1); }
+	char at(int index) const;
+	char & at(int index);
+
+	inline char * data() { return _data; }
+	inline const char * data() const { return _data; }
+	inline const char * constData() const { return _data; }
+
+	void clear();
+	void resize(int size, char character = '\0');
+
+	inline yByteArray & insert(int index, const yByteArray & other) { return insert(index, other._data, other._size); }
+	yByteArray & insert(int index, const char * data, int length);
+	inline yByteArray & insert(int index, const char * data) { return insert(index, data, strlen(data)); }
+	yByteArray & insert(int index, int count, char character);
+	inline yByteArray & insert(int index, char character) { return insert(index, 1, character); }
+
+	inline yByteArray & append(const yByteArray & other) { return append(other._data, other._size); }
+	inline yByteArray & append(const char * data, int length) { return insert(_size-1, data, length); }
+	inline yByteArray & append(const char * data) { return append(data, strlen(data)); }
+	inline yByteArray & append(int count, char character) { return insert(_size - 1, count, character); }
+	inline yByteArray & append(char character) { return append(1, character); }
+	inline yByteArray & push_back(const yByteArray & other) { return append(other); }
+	inline yByteArray & push_back(const char * data) { return append(data); }
+	inline yByteArray & push_back(char character) { return append(character); }
+
+	inline yByteArray & prepend(const yByteArray & other) { return prepend(other._data, other._size); }
+	inline yByteArray & prepend(const char * data, int length) { return insert(0, data, length); }
+	inline yByteArray & prepend(const char * data) { return prepend(data, strlen(data)); }
+	inline yByteArray & prepend(int count, char character) { return insert(0, count, character); }
+	inline yByteArray & prepend(char character) { return prepend(1, character); }
+	inline yByteArray & push_front(const yByteArray & other) { return prepend(other); }
+	inline yByteArray & push_front(const char * data) { return prepend(data); }
+	inline yByteArray & push_front(char character) { return prepend(character); }
+
+	void remove(int pos, int length);
+
+	yByteArray & fill(char character, int size = -1);
+
+private:
+	int _size;
+	char * _data;
+};
---
+++ yCore/source/yCore/yByteArray.cpp
@@ -0,0 +1,98 @@
+#include <yCore/yByteArray.h>
+
+char yByteArray::at(int index) const
+{
+	yASSERT_X(index >= 0 && index < _size, "yByteArray::back()", "out of range");
+
+	return *(_data + index);
+}
+char & yByteArray::at(int index)
+{
+	yASSERT_X(index >= 0 && index < _size, "yByteArray::back()", "out of range");
+
+	return *(_data + index);
+}
+
+void yByteArray::clear()
+{
+	free(_data);
+	_data = (char*) malloc(0);
+	_size = 0;
+}
+void yByteArray::resize(int size, char character)
+{
+	if (size != _size) {
+		_data = (char*)realloc(_data, size);
+		if (size > _size) {
+			for (int i(_size); i < size; ++i) {
+				_data[i] = character;
+			}
+		}
+		_size = size;
+	}
+}
+
+yByteArray & yByteArray::insert(int index, const char * data, int length)
+{
+	if (length > 0 && data) {
+		_size += length;
+		_data = (char*)realloc(_data, _size);
+
+		for (int i(_size); i >= index; --i) {
+			_data[i + length] = _data[i];
+		}
+
+		for (int i(index); i < index + length; ++i) {
+			_data[i] = data[index - length];
+		}
+	}
+
+	return *this;
+}
+yByteArray & yByteArray::insert(int index, int count, char character)
+{
+	yASSERT_X(index >= 0 && index <= _size, "yByteArray::insert()", "out of range");
+
+	if (count > 0) {
+		_size += count;
+		_data = (char*)realloc(_data, _size);
+
+		for (int i(_size); i >= index; --i) {
+			_data[i + count] = _data[i];
+		}
+
+		for (int i(index); i < index + count; ++i) {
+			_data[i] = character;
+		}
+	}
+
+	return *this;
+}
+
+yByteArray & yByteArray::fill(char character, int size)
+{
+	if (size >= 0) {
+		free(_data);
+		_data = (char*)malloc(size * sizeof(char));
+	}
+
+	for (int i(0); i < size; ++i) {
+		_data[i] = character;
+	}
+}
+
+void yByteArray::remove(int pos, int length)
+{
+	if (pos < 0 || pos >= _size && length <= 0)
+		return;
+
+	if (pos + length >= _size)
+		length = (pos + length) - _size - pos;
+
+	for (int i(pos + length); i < _size; ++i) {
+		_data[i - length] = _data[i];
+	}
+
+	_size -= length;
+	_data = (char*)realloc(_data, _size);
+}
