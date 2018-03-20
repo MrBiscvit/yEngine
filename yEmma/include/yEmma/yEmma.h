a543d7c8437d64f05b69d5b030e358f97d9d610d
MrBiscvit <mr.biscvityt@gmail.com>
2018-03-20 17:44:11 +0100

    Add yEmma project

---
+++ yEmma/include/yEmma/yEmma.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <yCore/yGlobal.h>
+
+#define yEMMA_MAJOR 1
+#define yEMMA_MINOR 0
+#define yEMMA_PATCH 0
+#define yEMMA_VERSION yVERSION(yEMMA_MAJOR, yEMMA_MINOR, yEMMA_PATCH)
+
+#if defined(yEMMA_STATIC)
+#  define yEMMA_API
+#else
+#  if defined(yEMMA_BUILD)
+#    define yEMMA_API yDECL_EXPORT
+#  else
+#    define yEMMA_API yDECL_IMPORT
+#  endif
+#endif
+
+#if defined(yUSE_OPENGL) && defined(yUSE_DIRECT3D)
+#  error
+#elif !defined(yUSE_OPENGL) && !defined(yUSE_DIRECT3D)
+#  define yUSE_OPENGL
+#endif
