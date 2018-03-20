d6d63faa5d988fee4f1ee3d75f5d05c7b4ae8b98
MrBiscvit <mr.biscvityt@gmail.com>
2018-03-20 17:45:52 +0100

    Add error message

--- yEmma/include/yEmma/yEmma.h
+++ yEmma/include/yEmma/yEmma.h
@@ -18,7 +18,7 @@
 #endif
 
 #if defined(yUSE_OPENGL) && defined(yUSE_DIRECT3D)
-#  error
+#  error You can only define one of these macros: yUSE_OPENGL & yUSE_DIRECT3D
 #elif !defined(yUSE_OPENGL) && !defined(yUSE_DIRECT3D)
 #  define yUSE_OPENGL
 #endif
