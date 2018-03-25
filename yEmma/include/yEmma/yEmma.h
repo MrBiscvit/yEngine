550ff5a51318464fe26de9860147f778d73ac69e
MrBiscvit <mr.biscvityt@gmail.com>
2018-03-25 20:48:25 +0200

    Add some preprocessor instructors for Direct3D and directly include OpenGL or Direct3D

--- yEmma/include/yEmma/yEmma.h
+++ yEmma/include/yEmma/yEmma.h
@@ -20,9 +20,21 @@
 #if defined(yUSE_OPENGL) && defined(yUSE_DIRECT3D)
 #  error You can only define one of these macros: yUSE_OPENGL or yUSE_DIRECT3D
 #elif !defined(yUSE_OPENGL) && !defined(yUSE_DIRECT3D)
-#  define yUSE_OPENGL
+#  if defined(yOS_WINDOWS)
+#    define yUSE_DIRECT3D
+#  else
+#    define yUSE_OPENGL
+#  endif
 #endif
 
 #if defined(yUSE_DIRECT3D)
-#  error DirectX is not supported for the moment!
+#  if !defined(yUSE_DIRECT3D_12) && !defined(yUSE_DIRECT3D_11) && !defined(yUSE_DIRECT3D_9)
+#    define yUSE_DIRECT3D_11
+#  endif
+#endif
+
+#if defined(yUSE_OPENGL)
+#include <yEmma/OpenGL/yOpenGL.h>
+#elif defined(yUSE_DIRECT3D)
+#include <yEmma/Direct3D/yDirect3D.h>
 #endif
