5eddf458f6f1fd5bba10d23b69be17682363a851
MrBiscvit <mr.biscvityt@gmail.com>
2018-04-07 23:04:33 +0200

    Add detection macros for OS

--- yCore/include/yCore/yConfig.h
+++ yCore/include/yCore/yConfig.h
@@ -26,6 +26,32 @@
 #define _YON_CONFIG_HEADER_
 #pragma once
 
+#if defined(_WIN32)
+#  define yOS_WINDOWS
+#elif defined(__APPLE__)
+#  define yOS_APPLE
+#  define yOS_UNIX
+#  include "TargetConditionals.h"
+#  if TARGET_IPHONE_SIMULATOR
+#    define yOS_IOS
+#    define yOS_IOS_SIMULATOR
+#  elif TARGET_OS_IPHONE
+#    define yOS_IOS
+#  elif TARGET_OS_MAC
+#    define yOS_MACOS
+#    define yOS_DARWIN
+#  else
+#    error "Unknown Apple Platform, please contact support!"
+#  endif
+#elif defined(__linux__)
+#  define yOS_LINUX
+#  define yOS_UNIX
+#elif defined(__unix__)
+#  define yOS_UNIX
+#else
+#  pragma warning("Unknown OS, please contact support!")
+#endif
+
 #if defined(_MSC_VER)
 // Microsoft Visual Studio Compiler
 #  define yCC_MVSC _MSC_VER
